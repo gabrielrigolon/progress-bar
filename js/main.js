b71dec6c486a2d7e0b44a00c66d983585c5dcd36
gabrielrigolon <gabrielrigolon@gmail.com>
2026-02-05 23:01:29 -0300

    finalizada primeira versao do card

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 import { renderProgress } from './progress.js';
 
 const progressState = {
-  current: 94,
+  current: 115,
   total: 1189
 };
 
@@ -15,3 +15,33 @@ function advance() {
     renderProgress(progressState);
   }
 }
+
+window.exportImage = async function () {
+  const node = document.getElementById('export-area');
+
+  await document.fonts.ready;
+
+  htmlToImage.toPng(node, {
+    pixelRatio: 2,
+    cacheBust: true,
+  })
+  .then((dataUrl) => {
+    const link = document.createElement('a');
+    link.download = 'progresso.png';
+    link.href = dataUrl;
+    link.click();
+  })
+  .catch((err) => {
+    console.error('Erro ao gerar imagem', err);
+  });
+};
+
+window.setMode = function (mode) {
+  const exportArea = document.getElementById('export-area');
+
+  if (mode === 'story') {
+    exportArea.classList.add('story');
+  } else {
+    exportArea.classList.remove('story');
+  }
+};
